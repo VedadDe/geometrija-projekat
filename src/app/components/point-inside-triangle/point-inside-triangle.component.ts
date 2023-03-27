147fd20f5a6033645c99cc421bfc2674874b62ce
VedadDe <96734106+VedadDe@users.noreply.github.com>
2023-03-27 23:08:53 +0200

    feat: tacka u trouglu

--- src/app/components/point-inside-triangle/point-inside-triangle.component.ts
+++ src/app/components/point-inside-triangle/point-inside-triangle.component.ts
@@ -1,12 +1,13 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 
-interface Point {
+interface Tacka {
   x: number;
   y: number;
 }
-class Point {
+class Tacka {
   constructor(public x: number, public y: number) {}
 }
+
 
 
 @Component({
@@ -15,113 +16,110 @@ class Point {
   styleUrls: ['./point-inside-triangle.component.scss']
 })
 export class PointInsideTriangleComponent {
-
+  
   @ViewChild('canvas') canvasRef!: ElementRef<HTMLCanvasElement>;
   ctx!: CanvasRenderingContext2D;
-  points: Point[] = [];
-  showModal = false;
-  isInside = false;
+  tacke: Tacka[] = [];
+  prikaziModal = false;
+  unutar = false;
 
-  addPoint(event: MouseEvent) {
+  dodajTacku(event: MouseEvent) {
    const canvas = this.canvasRef.nativeElement;
-   const rect = canvas.getBoundingClientRect();
-   const x = event.clientX - rect.left;
-   const y = event.clientY - rect.top;
+   const pravougaonik = canvas.getBoundingClientRect();
+   const x = event.clientX - pravougaonik.left;
+   const y = event.clientY - pravougaonik.top;
  
-   const point = new Point(x, y);
-   this.points.push(point);
+   const tacka = new Tacka(x, y);
+   this.tacke.push(tacka);
  
-   // Draw point
+   // Crtaj tačku
    this.ctx.beginPath();
-   this.ctx.arc(point.x, point.y, 5, 0, 2 * Math.PI);
+   this.ctx.arc(tacka.x, tacka.y, 5, 0, 2 * Math.PI);
    this.ctx.fillStyle = 'red';
    this.ctx.fill();
    this.ctx.stroke();
  
-   if (this.points.length === 3) {
-     this.drawTriangle();
+   if (this.tacke.length === 3) {
+     this.crtajTrokut();
    }
  }
  
 
  ngAfterViewInit() {
 
-     // Get canvas element and context
+     // Dohvati canvas element i kontekst
      const canvas = this.canvasRef.nativeElement;
      canvas.width = window.innerWidth;
      canvas.height = window.innerHeight;
      this.ctx = canvas.getContext('2d')!;
-     this.drawTriangle();
+     this.crtajTrokut();
  }
  
 
  canvasClick(event: MouseEvent) {
-   const canvas = this.canvasRef.nativeElement;
-   const rect = canvas.getBoundingClientRect();
-   // const x = event.clientX - rect.left;
-   // const y = event.clientY - rect.top;
+  const canvas = this.canvasRef.nativeElement;
+  const pravougaonik = canvas.getBoundingClientRect();
 
+  // Napravi objekat tačke iz klika
+  const tacka = { x: event.offsetX, y: event.offsetY };
 
-   // Create a point object from the click coordinates
-   const point = { x: event.offsetX, y: event.offsetY };
+  //
+  this.ctx.beginPath();
+  this.ctx.arc(tacka.x, tacka.y, 5, 0, 2 * Math.PI);
+  this.ctx.fillStyle = 'red';
+  this.ctx.fill();
+  this.ctx.stroke();
+  // Dodaj tačku u niz tačaka
+  this.tacke.push(tacka);
 
-   //
-   this.ctx.beginPath();
-   this.ctx.arc(point.x, point.y, 5, 0, 2 * Math.PI);
-   this.ctx.fillStyle = 'red';
-   this.ctx.fill();
-   this.ctx.stroke();
-   // Add the point to the points array
-   this.points.push(point);
+  // Ako su tri tačke kliknute, nacrtaj trokut i dodaj osluškivač događaja za četvrti klik
+  if (this.tacke.length === 3) {
+    this.crtajTrokut();
 
-   // If three points have been clicked, draw the triangle and add an event listener for the fourth click
-   if (this.points.length === 3) {
-     this.drawTriangle();
+    // Dodaj osluškivač događaja za četvrti klik
+    const canvas = this.canvasRef.nativeElement;
+    canvas.addEventListener('click', (e: MouseEvent) => {
+      // Napravi objekat tačke iz klika
+      const tacka = { x: e.offsetX, y: e.offsetY };
 
-     // Add an event listener for the fourth click
-     const canvas = this.canvasRef.nativeElement;
-     canvas.addEventListener('click', (e: MouseEvent) => {
-       // Create a point object from the click coordinates
-       const point = { x: e.offsetX, y: e.offsetY };
+      // Provjeri da li je tačka unutar ili izvan trokuta
+      this.unutar = this.jeLiTackaUnutarTrokuta(tacka);
 
-       // Check if the point is inside or outside the triangle
-       this.isInside = this.isPointInsideTriangle(point);
+      // Prikaži modal sa rezultatom
+      this.prikaziModal = true;
+    });
+  }
+}
 
-       // Show the modal with the result
-       this.showModal = true;
-     });
-   }
- }
+crtajTrokut() {
+  this.ctx.beginPath();
+  this.ctx.moveTo(this.tacke[0].x, this.tacke[0].y);
+  this.ctx.lineTo(this.tacke[1].x, this.tacke[1].y);
+  this.ctx.lineTo(this.tacke[2].x, this.tacke[2].y);
+  this.ctx.closePath();
+  this.ctx.stroke();
+}
 
- drawTriangle() {
-   this.ctx.beginPath();
-   this.ctx.moveTo(this.points[0].x, this.points[0].y);
-   this.ctx.lineTo(this.points[1].x, this.points[1].y);
-   this.ctx.lineTo(this.points[2].x, this.points[2].y);
-   this.ctx.closePath();
-   this.ctx.stroke();
- }
+racunajPovrsinuTrokuta(p1: Tacka, p2: Tacka, p3: Tacka) {
+  return Math.abs((p1.x * (p2.y - p3.y) + p2.x * (p3.y - p1.y) + p3.x * (p1.y - p2.y)) / 2);
+}
 
- calcTriangleArea(p1: Point, p2: Point, p3: Point) {
-   return Math.abs((p1.x * (p2.y - p3.y) + p2.x * (p3.y - p1.y) + p3.x * (p1.y - p2.y)) / 2);
- }
+jeLiTackaUnutarTrokuta(p: Tacka) {
+  // Izračunaj površinu trokuta koju čini tačka i svaki rub trokuta
+  const povrsina1 = this.racunajPovrsinuTrokuta(p, this.tacke[0], this.tacke[1]);
+  const povrsina2 = this.racunajPovrsinuTrokuta(p, this.tacke[1], this.tacke[2]);
+  const povrsina3 = this.racunajPovrsinuTrokuta(p, this.tacke[2], this.tacke[0]);
 
- isPointInsideTriangle(p: Point) {
-   // Calculate the area of the triangle formed by the point and each edge of the triangle
-   const area1 = this.calcTriangleArea(p, this.points[0], this.points[1]);
-   const area2 = this.calcTriangleArea(p, this.points[1], this.points[2]);
-   const area3 = this.calcTriangleArea(p, this.points[2], this.points[0]);
+  // Izračunaj ukupnu površinu trokuta
+  const ukupnaPovrsina = this.racunajPovrsinuTrokuta(this.tacke[0], this.tacke[1], this.tacke[2]);
 
-   // Calculate the total area of the triangle
-   const totalArea = this.calcTriangleArea(this.points[0], this.points[1], this.points[2]);
+  // Provjeri da li je zbir površina trokuta koje čini tačka i svaki rub jednak ukupnoj površini trokuta
+  return (povrsina1 + povrsina2 + povrsina3) === ukupnaPovrsina;
+}
 
-   // Check if the sum of the areas of the triangles formed by the point and each edge is equal to the total area of the triangle
-   return (area1 + area2 + area3) === totalArea;
- }
-
- closeModal() {
-   this.showModal = false;
-   this.points = [];
-   this.ctx.clearRect(0, 0, this.canvasRef.nativeElement.width, this.canvasRef.nativeElement.height);
- }
+zatvoriModal() {
+  this.prikaziModal = false;
+  this.tacke = [];
+  this.ctx.clearRect(0, 0, this.canvasRef.nativeElement.width, this.canvasRef.nativeElement.height);
+}
 }
